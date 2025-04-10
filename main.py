f19e3c02656f829be726b8f6f4a3325adb4e3545
Gennadii Ershov <gennadiialbershov@gmail.com>
2025-04-09 23:28:46 -0400

    feat: add run function

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from label import get_sentiment
 from visualize import make_plot
-
 import json
 
 
@@ -9,20 +8,17 @@ def run(filepath: str):
     INSERT DOCSTRING HERE
     """
     # open the json object
-    ...
+    with open(filepath) as file:
+        data = json.load(file)
 
-    # extract the reviews from the json file
-    ...
-
+    reviews = data['results']
+    
     # get a list of sentiments for each line using get_sentiment
-    ...
-
+    sentiments = get_sentiment(reviews)
+    
     # plot a visualization expressing sentiment ratio
-    ...
-
-    # return sentiments
-    ...
-
+    make_plot(sentiments)
+    return sentiments
 
 if __name__ == "__main__":
     print(run("data/raw/reviews.json"))
