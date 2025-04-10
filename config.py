bc14941b2792f5dac95611674dbe5d2ad8cce69f
Gennadii Ershov <gennadiialbershov@gmail.com>
2025-04-09 23:23:16 -0400

    fix: update error message capitalization in config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 out_list = ["negative", "positive", "positive", "neutral", "irrelevant",
             "negative"]
 
-error_message = "Wrong input. text must be an array of strings."
+error_message = "Wrong input. Text must be an array of strings."
