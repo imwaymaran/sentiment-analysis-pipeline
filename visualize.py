dceead3f3273ff8d0c976d89e05b51b8b388556e
Gennadii Ershov <gennadiialbershov@gmail.com>
2025-04-09 22:26:57 -0400

    docs: add docstring to make_plot function

--- visualize.py
+++ visualize.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 from collections import Counter 
 
 def make_plot(sentiments: list) -> None:
-    """_summary_
+    """Plots and saves a bar chart of sentiment counts to 'images/sentiments.png'.
 
     Args:
-        sentiments (list): _description_
+        sentiments (list of str): A list of sentiment labels.
     """
     sentiments_cnt = Counter(sentiments)    
 
