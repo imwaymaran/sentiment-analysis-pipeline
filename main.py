a8fe26720ed46fa8dc2064d27f729d3dced4a8f9
Gennadii Ershov <gennadiialbershov@gmail.com>
2025-04-09 23:32:22 -0400

    docs: add docstring to run function

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from visualize import make_plot
 import json
 
 
-def run(filepath: str):
+def run(filepath: str) -> list:
     """
-    INSERT DOCSTRING HERE
+    Processes review data from a JSON file, analyzes sentiment, and visualizes the result.
+    
+    Args:
+        filepath (str): The path to the JSON file containing the review data.
+
+    Returns:
+        list of str: A list of sentiment labels for the reviews.
     """
+    
     # open the json object
     with open(filepath) as file:
         data = json.load(file)
